823e6d09e832610eaab39109a67d666a24310f1b
Russell Haering <russellhaering@gmail.com>
2024-05-15 08:40:03 -0700

    Print CPU info

--- float_test.go
+++ float_test.go
@@ -5,10 +5,14 @@ import (
 )
 
 var (
+	// expected is the value returned most often by the function(s) in testing, I've made
+	// no attempt to confirm that it's actually "correct".
 	expected = float64(25.196850393700785)
 )
 
 func TestDoingMath(t *testing.T) {
+	PrintCPUInfo()
+
 	fns := map[string]func() float64{
 		"FirstWay":  FirstWay,
 		"SecondWay": SecondWay,
--- math.go
+++ math.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/shirou/gopsutil/v3/cpu"
+)
 
 var (
 	foo = 10
@@ -22,7 +27,19 @@ func SecondWay() float64 {
 	return 20 + b*a
 }
 
+func PrintCPUInfo() {
+	cpuInfo, err := cpu.Info()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	spew.Dump(cpuInfo)
+}
+
 func main() {
+	PrintCPUInfo()
+
+	fmt.Println("-- Doing Math --")
 	fmt.Println("First Way: ", FirstWay())
 	fmt.Println("Second Way: ", SecondWay())
 }
